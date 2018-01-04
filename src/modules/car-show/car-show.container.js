29d7c464c9f9129201db2803757f3f493e5360a6
Levi Fuller <lfuller941@gmail.com>
2018-01-03 21:15:37 -0700

    Playing around

--- src/modules/car-show/car-show.container.js
+++ src/modules/car-show/car-show.container.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { If } from "../common/utilities";
 import {Header} from 'semantic-ui-react';
 import CarList from './car-list/car-list.component';
+import CarEditor from './car-editor/car-editor.component';
+
+const styles = {
+  header: {
+    textAlign: 'center',
+    
+  }
+  
+}
 
 // Stateful Component
 class CarShow extends React.Component {
@@ -14,8 +23,10 @@ class CarShow extends React.Component {
         <If condition={true}>
           <CarList />
         </If>
+        <CarEditor />
       </div>
     );
   }
 }
 export default CarShow;
+
